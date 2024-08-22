1d3c21f387c670408ddf7a92cc9a3e8a6b4786d6
Joel Bremner <jbr257@uclive.ac.nz>
2024-08-22 16:45:08 +1200

    Now if no flash cards exist, it prompts you to create one

--- FlashCardApp/app/src/main/java/nz/ac/canterbury/seng303/lab2/screens/PlayQuizScreen.kt
+++ FlashCardApp/app/src/main/java/nz/ac/canterbury/seng303/lab2/screens/PlayQuizScreen.kt
@@ -1,13 +1,21 @@
 package nz.ac.canterbury.seng303.lab2.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 
@@ -24,8 +32,42 @@ fun PlayQuizScreen(navController: NavController, quizViewModel: QuizViewModel = 
     val showSummary by quizViewModel.showSummary.collectAsState()
     val userAnswers by quizViewModel.userAnswers.collectAsState()
 
-    // Display the current flashcard or the summary
-    if (!showSummary) {
+    if (flashCards.isEmpty()) {
+        val padding = 16.dp
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "No Flash Cards Try Creating One",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                ),
+                modifier = Modifier.padding(bottom = padding)
+            )
+            Button(
+                onClick = { navController.navigate("CreateFlashCard") },
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Text("Create Flash Card")
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "Add",
+                        modifier = Modifier.size(16.dp)
+                    )
+                }
+            }
+        }
+    } else if (!showSummary) {
         val currentFlashcard = flashCards.getOrNull(currentIndex)
         currentFlashcard?.let {
             Column(modifier = Modifier.padding(16.dp)) {
